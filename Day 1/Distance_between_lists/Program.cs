4aad4469c4519232eb0ca329a7515a3393ca4014
Simphiwe <u19027372@tuks.co.za>
2025-04-05 16:19:02 +0200

    🌱  add base code (reading from a file)

--- Day 1/Distance_between_lists/Program.cs
+++ Day 1/Distance_between_lists/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        var path = "input.txt";
+
+        if (!File.Exists(path))
+        {
+            Console.WriteLine("Missing input file.");
+            return;
+        }
+
+        try
+        {
+            var lines = File.ReadAllLines(path);
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                
+
+            }
+
+           
+           
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Oops! Something went wrong:");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
