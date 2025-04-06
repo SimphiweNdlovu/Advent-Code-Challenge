75fed49e23ab1e2d674e8d5df838ff1fb1e7123f
Simphiwe <u19027372@tuks.co.za>
2025-04-06 18:42:02 +0200

    🚧 base line code

--- Day 4/Day4_Part1/Program.cs
+++ Day 4/Day4_Part1/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        var path = "input.txt";
+
+        if (!File.Exists(path))
+        {
+            return;
+        }
+
+        string input = File.ReadAllText(path);
+        var lines = input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        int wordCount = 0;
+        string word = "XMAS";
+        int wordLength = word.Length;
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            for (int j = 0; j < lines[i].Length; j++)
+            {
+                wordCount += CountWordInDirections(lines, word, i, j, wordLength);
+            }
+        }
+
+        Console.WriteLine(wordCount);
+    }
+
+    static int CountWordInDirections(string[] grid, string word, int row, int col, int wordLength)
+    {
+        int count = 0;
+
+        return count;
+    }
+
+    static bool IsWordInDirection(string[] grid, string word, int row, int col, int dx, int dy, int wordLength)
+    {
+        
+        return true;
+    }
+}
