ae73b91364bdf84b6024577a2b51d5a980b54355
Simphiwe <u19027372@tuks.co.za>
2025-04-07 23:59:44 +0200

    ✨ Implement Day 9 Part 1 logic for processing input data and calculating checksum

--- Day 9/Day9_Part1/Program.cs
+++ Day 9/Day9_Part1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+class Program {
+    static void Main() {
+        var path = "input.txt";
+        if (!File.Exists(path)) {
+            Console.WriteLine("Missing input file.");
+            return;
+        }
+        string input = File.ReadAllText(path).Trim();
+        List<int> disk = new List<int>();
+        bool isFile = true;
+        int fileID = 0;
+        foreach (char c in input) {
+            int count = c - '0';
+            if (isFile) {
+                for (int i = 0; i < count; i++) disk.Add(fileID);
+                fileID++;
+            } else {
+                for (int i = 0; i < count; i++) disk.Add(-1);
+            }
+            isFile = !isFile;
+        }
+        while (true) {
+            int left = disk.FindIndex(x => x == -1);
+            if (left == -1) break;
+            int right = -1;
+            for (int i = disk.Count - 1; i > left; i--) {
+                if (disk[i] != -1) { right = i; break; }
+            }
+            if (right == -1 || right <= left) break;
+            disk[left] = disk[right];
+            disk[right] = -1;
+        }
+        long checksum = 0;
+        for (int i = 0; i < disk.Count; i++) {
+            if (disk[i] != -1)
+                checksum += (long)i * disk[i];
+        }
+        Console.WriteLine(checksum);
+    }
+}
